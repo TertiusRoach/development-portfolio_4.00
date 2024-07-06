e5d55262096b399e8a4816767662bbcb51568f19
Tertius Roach <tertius.roach@gmail.com>
2024-07-06 15:36:00 +0200

    Todo: Test Live

--- public/index.js
+++ public/index.js
@@ -1,2 +1,2 @@
-(()=>{var o,l;o=window.location.pathname.split("/"),l=o[o.length-1],console.log(l)})();
+(()=>{"use strict";var o,l=(o="<main>",console.log(o),"");console.log(l)})();
 //# sourceMappingURL=index.js.map
