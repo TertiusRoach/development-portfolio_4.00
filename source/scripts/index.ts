1147df0b554834b89fc8ab8482eb00dd9af8deb7
Tertius Roach <tertius.roach@gmail.com>
2024-07-19 13:18:20 +0200

    Reordered Functions

--- source/scripts/index.ts
+++ source/scripts/index.ts
@@ -26,6 +26,7 @@ export function getResolution(): String {
 
   return resolution as String;
 }
+
 export function setActive(button: HTMLButtonElement, blockName: String): void {
   const activeButton = document.querySelector(`#${blockName}-active`) as HTMLElement;
   if (button.parentElement?.tagName === 'MENU') {
@@ -38,7 +39,47 @@ export function setActive(button: HTMLButtonElement, blockName: String): void {
     button.id = `${blockName}-active`;
   }
 }
-export function showSection(pageName: string, blockName: 'overlay' | string) {
+export function setMenu(blockName: String, labelName: String) {
+  // Get currently active section and the selected section elements
+  const disableElement = document.getElementById('main-active') as HTMLElement | null;
+  const activateElement = document.querySelector(`.${blockName}-${labelName}`) as HTMLElement | null;
+
+  // Helper function to update active elements
+  const toggleElement = function (
+    disableElement: HTMLElement | HTMLButtonElement | null,
+    activateElement: HTMLElement | HTMLButtonElement | null,
+    id: string
+  ) {
+    if (disableElement) {
+      disableElement.removeAttribute('id');
+    }
+    if (activateElement) {
+      activateElement.setAttribute('id', id);
+    }
+  };
+
+  // Determine orientation and update the active button accordingly
+  let block: 'header' | 'footer' | '';
+  switch (getOrientation()) {
+    case 'desktop-landscape':
+      block = 'header';
+      break;
+    case 'mobile-portrait':
+      block = 'footer';
+      break;
+    default:
+      block = '';
+  }
+
+  if (block) {
+    let activeButton = document.querySelector(`#${block}-active`) as HTMLButtonElement;
+    let selectButton = document.querySelector(`.${block}-${labelName}`) as HTMLButtonElement;
+    toggleElement(activeButton, selectButton, `${block}-active`); // Update the <header> or <footer> element based on device orientation
+    toggleElement(disableElement, activateElement, 'main-active'); // Activate the <section> inside the <main> container
+  }
+}
+
+export function showSection(pageName: string, blockName: 'overlay' | String) {
   const element = document.getElementById('index-overlay') as HTMLElement;
   if (!element) {
     console.error('Element with ID "index-overlay" not found.');
@@ -119,6 +160,7 @@ export function showAside(blockName: 'leftbar' | 'rightbar' | string) {
     }
   }
 }
+
 export function scrollInfo(button: HTMLButtonElement, container: HTMLElement, blockName: 'header' | 'footer' | String) {
   const setPixels = function (container: HTMLElement): { className: string; scrollAmount: number }[] {
     let children = Array.from(container.children) as HTMLElement[]; //--|🠈 Convert the container's children to an array of HTMLElement 🠈|--//
@@ -150,44 +192,5 @@ export function scrollInfo(button: HTMLButtonElement, container: HTMLElement, bl
       scrollTag: scrollTag as String,
       scrollTop: scrollPixels.scrollAmount as Number,
     };
-  }
-}
-export function synchronizeNavigation(blockName: String, labelName: String) {
-  // Get currently active section and the selected section elements
-  const disableElement = document.getElementById('main-active') as HTMLElement | null;
-  const activateElement = document.querySelector(`.${blockName}-${labelName}`) as HTMLElement | null;
-
-  // Helper function to update active elements
-  const toggleElement = function (
-    disableElement: HTMLElement | HTMLButtonElement | null,
-    activateElement: HTMLElement | HTMLButtonElement | null,
-    id: string
-  ) {
-    if (disableElement) {
-      disableElement.removeAttribute('id');
-    }
-    if (activateElement) {
-      activateElement.setAttribute('id', id);
-    }
-  };
-
-  // Determine orientation and update the active button accordingly
-  let block: 'header' | 'footer' | '';
-  switch (getOrientation()) {
-    case 'desktop-landscape':
-      block = 'header';
-      break;
-    case 'mobile-portrait':
-      block = 'footer';
-      break;
-    default:
-      block = '';
-  }
-
-  if (block) {
-    let activeButton = document.querySelector(`#${block}-active`) as HTMLButtonElement;
-    let selectButton = document.querySelector(`.${block}-${labelName}`) as HTMLButtonElement;
-    toggleElement(activeButton, selectButton, `${block}-active`); // Update the <header> or <footer> element based on device orientation
-    toggleElement(disableElement, activateElement, 'main-active'); // Activate the <section> inside the <main> container
   }
 }
