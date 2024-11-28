e7ce2c56784e1cfc608fbc52b72ff5909ef5e46f
Tertius Roach <tertius.roach@gmail.com>
2024-11-28 12:51:32 +0200

    Next Step: Build user login using MongoDB without breaching security.

--- source/modules/server/data.js
+++ source/modules/server/data.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
 const { MongoClient } = require('mongodb');
 
+dotenv.config(); // Load variables from .env
 let databaseConnection;
 
 module.exports = {
   connectToDatabase: (callback) => {
-    const uri = process.env.MONGODB_URI; // Access the URI from environment variables
+    const uri = process.env.USERS_URI; // Access the URI from environment variables
+
+    // const uri = process.env.BOOKSTORE_URI; // Access the URI from environment variables
     // const uri = 'mongodb://localhost:27017/bookstore';
 
     MongoClient.connect(uri)
