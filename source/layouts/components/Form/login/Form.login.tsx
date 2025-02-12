14f3417b9f062ba697bf9c296be5bd6bc3a0a97c
Tertius Roach <tertius.roach@gmail.com>
2025-02-12 15:09:48 +0200

    This page is messy, might have to neaten up this one a bit.

--- source/layouts/components/Form/login/Form.login.tsx
+++ source/layouts/components/Form/login/Form.login.tsx
@@ -78,18 +78,21 @@ const FormLogin: React.FC<InfoProps> = ({ info }) => {
     } catch (error) {
       //--|🠊 Handle Login Errors 🠈|--//
       const axiosError = error as AxiosError;
-
+      let dialogue: string;
       if (axiosError.response?.status === 404) {
         //--|🠊 User not found 🠈|--//
-        let tutorial: string = "It looks like you're new here. Please sign up to get started.";
+        dialogue = "It looks like you're new here. Please sign up to get started.";
+
         viewCarousel('register'); //--|🠈 Redirect to the registration page 🠈|--//
-        toggleText('.register-text', tutorial); //--|🠈 Provide guidance for new users 🠈|--//
+        toggleText('.register-text', dialogue); //--|🠈 Provide guidance for new users 🠈|--//
       } else if (axiosError.response?.status === 401) {
         //--|🠊 Invalid password 🠈|--//
-        let tutorial: string = 'Forgot your password? You can reset it here.';
-        // alert('Invalid password. Redirecting to password reset section.');
+        dialogue = 'Forgot your password? You can reset it here.';
+
+        /* alert('Invalid password. Redirecting to password reset section.'); */
+
         viewCarousel('password'); //--|🠈 Redirect to password reset section 🠈|--//
-        toggleText('.password-text', tutorial); //--|🠈 Provide guidance for registered users 🠈|--//
+        toggleText('.password-text', dialogue); //--|🠈 Provide guidance for registered users 🠈|--//
       } else {
         alert('An error occurred during login. Please try again later.');
         console.error('Error during login:', error); //--|🠈 Log unexpected errors for debugging 🠈|--//
