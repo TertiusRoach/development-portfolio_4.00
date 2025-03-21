7eba320d706855500d748c52907a3aae07fb02c2
Tertius Roach <tertius.roach@gmail.com>
2025-03-21 15:13:29 +0200

    Added a Button.sharp Component

--- source/layouts/components/Article/selection/Article.selection.tsx
+++ source/layouts/components/Article/selection/Article.selection.tsx
@@ -6,9 +6,12 @@ import './Article.selection.scss';
 import React, { useEffect, useState, createContext, useContext } from 'react';
 //--|🠉 Dependencies 🠉|--//
 //--|🠋 Components 🠋|--//
+import ButtonSharp from '../../Button/sharp/Button.sharp';
 import MenuBranding from '../../Menu/branding/Menu.branding';
 //--|🠉 Components 🠉|--//
 //--|🠋 Functions 🠋|--//
+import { defineButton } from './Article_selection';
+import { viewBlock, viewText, axiosError } from '../../../../landing';
 //--|🠉 Functions 🠉|--//
 
 interface InfoProps {
@@ -25,7 +28,6 @@ const ArticleSelection: React.FC<InfoProps> = ({ info }) => {
   useEffect(() => {}, [pageName, blockName]);
   let brandImage =
     'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/861d6c3d25d45ec174c8d12aedc407f59dc85317/source/assets/svg-files/trinity-apps/trinity-apps.svg';
-
   let demoImage =
     'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/7e2882f29c5a3697900192c18bec75aa5916b207/source/assets/svg-files/landing-page/laptop.svg';
   let appsImage =
@@ -53,9 +55,32 @@ const ArticleSelection: React.FC<InfoProps> = ({ info }) => {
           <img style={{ maskImage: `url(${demoImage})`, WebkitMaskImage: `url(${demoImage})` }} />
         </menu>
       </aside>
-      <nav className="open-apps">{/* <ul className="open-apps"></ul> */}</nav>
-      <nav className="view-demo">{/* <ul className="view-demo"></ul> */}</nav>
-      <div className="selection-icon">
+
+      <nav className="navigation-apps"></nav>
+      <nav className="navigation-demos">
+        <ul className="view-demo">
+          <ButtonSharp
+            type="button"
+            text={'Track a Day'}
+            // onClick={() => viewBlock('register')}
+            style={defineButton('track-day', { pageName, blockName })}
+          />
+          <ButtonSharp
+            type="button"
+            text={'Log a Ticket'}
+            // onClick={() => viewBlock('register')}
+            style={defineButton('log-ticket', { pageName, blockName })}
+          />
+          <ButtonSharp
+            type="button"
+            text={'Find a Link'}
+            // onClick={() => viewBlock('register')}
+            style={defineButton('find-link', { pageName, blockName })}
+          />
+        </ul>
+      </nav>
+
+      <div className="branding-icon">
         <img style={{ maskImage: `url(${brandImage})`, WebkitMaskImage: `url(${brandImage})` }} />
       </div>
     </article>
---
+++ source/layouts/components/Article/selection/Article_selection.ts
@@ -0,0 +1,42 @@
+export function defineButton(
+  button: 'track-day' | 'log-ticket' | 'find-link',
+  info: { blockName: string; pageName: string }
+) {
+  const { blockName, pageName } = info;
+  //--|🠋 Always Return an Object 🠋|--//
+  switch (button) {
+    case 'track-day':
+      return {
+        fontSize: '<h3>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: 'center' as 'left' | 'right' | 'center' | 'icon' | 'text',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'overtime' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+    case 'log-ticket':
+      return {
+        fontSize: '<h3>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: 'center' as 'left' | 'right' | 'center' | 'icon' | 'text',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'ticketing' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+    case 'find-link':
+      return {
+        fontSize: '<h3>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: 'center' as 'left' | 'right' | 'center' | 'icon' | 'text',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'hyperlink' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+  }
+}
