bf1c655cf5c01b1e0d7d3bc8207d0ff4688f79a2
Tertius Roach <tertius.roach@gmail.com>
2025-04-01 14:50:43 +0200

    Added TicketingFooter Container.

---
+++ source/layouts/containers/Footer/TicketingFooter/TicketingFooter.tsx
@@ -0,0 +1,33 @@
+//--|🠊 TicketingFooter.tsx 🠈|--//
+//--|🠋 Dependencies 🠋|--//
+import ReactDOM from 'react-dom/client';
+import axios, { AxiosError } from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import React, { useState, useEffect } from 'react';
+//--|🠉 Dependencies 🠉|--//
+//--|🠋 Components 🠋|--//
+//--|🠉 Components 🠉|--//
+
+interface InfoProps {
+  info: {
+    resolution: string;
+    orientation: 'desktop-landscape' | 'mobile-portrait' | 'tablet-square' | string;
+    identification: 'landing' | 'overtime' | 'ticketing' | 'hyperlink';
+  };
+}
+const TicketingFooter: React.FC<InfoProps> = ({ info }) => {
+  const blockName: string = 'footer';
+  const pageName: string = info.identification as 'overtime';
+  const stateName: 'expanded' | 'collapsed' = 'collapsed';
+
+  useEffect(() => {}, [pageName, blockName]);
+
+  return (
+    <footer className={`default-${blockName} collapsed`} id={`${pageName}-${blockName}`} style={{ zIndex: 1 }}>
+      <menu></menu>
+      <section></section>
+    </footer>
+  );
+};
+export default TicketingFooter;
