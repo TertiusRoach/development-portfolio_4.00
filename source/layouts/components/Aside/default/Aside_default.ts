d9cd8ad0eee1a72708262bcf2b1df16a249f452b
Tertius Roach <tertius.roach@gmail.com>
2025-06-29 09:23:02 +0200

    *Hand-Heart-Emoji*

---
+++ source/layouts/components/Aside/default/Aside_default.ts
@@ -0,0 +1,57 @@
+//--|🠊 Section_buttons.ts 🠈|--//
+export function defineButton(info: { pageName: string; blockName: string }) {
+  const { pageName, blockName } = info;
+  //--|🠋 Always Return an Object 🠋|--//
+  /*
+  switch(pageName){
+    
+  }
+  */
+  /*
+  switch (button) {
+    case 'track-day':
+      return {
+        fontSize: '<h1>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: '-text-' as '-left-' | '-right-' | '-center-' | '-icon-' | '-text-',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'overtime' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+    case 'log-ticket':
+      return {
+        fontSize: '<h1>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: '-text-' as '-left-' | '-right-' | '-center-' | '-icon-' | '-text-',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'ticketing' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+    case 'find-link':
+      return {
+        fontSize: '<h1>' as '<h1>' | '<h2>' | '<h3>' | '<h4>' | '<h5>' | '<h6>' | '<p>',
+        layoutView: '-text-' as '-left-' | '-right-' | '-center-' | '-icon-' | '-text-',
+        shadingView: 'dark' as 'dark' | 'medium' | 'light',
+
+        className: button,
+        blockName: blockName as 'main',
+        pageName: 'hyperlink' as 'landing' | 'overtime' | 'ticketing' | 'hyperlink',
+        imageLink: '',
+      };
+  }
+  */
+}
+
+export function sizeDivs(pageName: string, blockName: string) {
+  const mainBlock = document.querySelector(`#${pageName}-${blockName}`) as HTMLElement;
+  const mainDivs = mainBlock.querySelectorAll('section div[class*="size"]') as NodeListOf<HTMLDivElement>;
+  for (let i = 0; i < mainDivs.length; i++) {
+    let mainDiv = mainDivs[i] as HTMLDivElement;
+    mainDiv.style.width = `${mainBlock.offsetWidth}px`;
+    mainDiv.style.height = `${mainBlock.offsetHeight}px`;
+  }
+}
