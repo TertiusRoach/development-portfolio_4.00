e6b840c2c9ec9533e1d52158342719adffe6144e
Tertius Roach <63373022+TertiusRoach@users.noreply.github.com>
2026-04-09 10:22:55 +0200

    Starting the habit of logging inside the console to not get lost or sidetracked.

--- source/index.tsx
+++ source/index.tsx
@@ -1,14 +1,62 @@
 //--|🠊 index.tsx (Entry Point) 🠈|--\\
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 //--|🠋 Styles 🠋|--\\
 import './index.scss';
-//--|🠋 Containers 🠋|--\\
+
+//--|🠋 React 🠋|--\\
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//--|🠋 Applications 🠋|--\\
 import Buttons from './layouts/pages/buttons';
 import Landing from './layouts/pages/landing';
 import Overtime from './layouts/pages/overtime';
 import Ticketing from './layouts/pages/ticketing';
 import Hyperlink from './layouts/pages/hyperlink';
+
+//--|🠋 Functions 🠋|--\\
+function viewPage(pageName: 'overtime' | 'ticketing' | 'hyperlink' | 'buttons' | 'landing') {
+  const element = document.querySelector(`#${pageName}-body`) as HTMLDivElement; //--|🠈 Select the new view element using its dynamic ID 🠈|--\\
+  switch (true) {
+    case element.classList.contains('asleep'):
+      //--|🠉 Show if element is hidden 🠈|--\\
+      element.classList.remove('asleep'); //--|🠈 Remove '.asleep' 🠈|--\\
+      console.log(`|🠊 View: <div id="${pageName}-body"> 🠈|`);
+
+      return element.classList.add('active'); //--|🠈 Toggle '.active' 🠈|--\\
+    case element.classList.contains('active'):
+      //--|🠉 Optional toggle: allow hiding the current element again 🠈|--\\
+      element.classList.remove('active'); //--|🠈 Remove '.active' 🠈|--\\
+      return element.classList.add('asleep'); //--|🠈 Toggle '.asleep' 🠈|--\\
+  }
+}
+function loadPage(identification: string, container: React.ReactElement) {
+  const element = document.getElementById(identification) as HTMLDivElement;
+  if (!element) {
+    console.error(`Can't find #${identification}`);
+    return;
+  }
+  if (element.innerHTML === '') {
+    ReactDOM.createRoot(element).render(<React.StrictMode>{container}</React.StrictMode>);
+  } else {
+    console.warn(`Element #${identification} is not empty. Skipping render to avoid overwrite.`);
+  }
+}
+function themePage(colorScheme: 'light' | 'dark') {
+  const element: HTMLBodyElement = document.getElementsByTagName('body')[0];
+
+  switch (colorScheme) {
+    case 'light':
+      console.log('|🠊 Theme: <body class="ante-meridiem"> 🠈|');
+      return element.classList.add('ante-meridiem');
+    case 'dark':
+      console.log('|🠊 Theme: <body class="post-meridiem"> 🠈|');
+      return element.classList.add('post-meridiem');
+  }
+}
+setTimeout(() => {
+  themePage('light');
+  viewPage('overtime');
+}, 250);
 
 //--|🠋 Component Mapping 🠋|--\\
 const pages: { [key: string]: React.ElementType } = {
@@ -20,54 +68,24 @@ const pages: { [key: string]: React.ElementType } = {
 };
 Object.entries(pages).forEach(([id]) => {
   //--|🠋 Render Components 🠋|--\\
-  const elementBody = document.getElementById(id) as HTMLDivElement;
-  const pageName: string = elementBody.id.split('-')[0] as 'landing' | 'buttons' | 'overtime' | 'ticketing' | 'hyperlink';
+  const container = document.getElementById(id) as HTMLDivElement;
+  const pageName: string = container.id.split('-')[0] as 'landing' | 'buttons' | 'overtime' | 'ticketing' | 'hyperlink';
   switch (pageName) {
     case 'buttons':
-      safeLoad(`${pageName}-body`, React.createElement(Buttons));
+      loadPage(`${pageName}-body`, React.createElement(Buttons));
       break;
     case 'overtime':
-      safeLoad(`${pageName}-body`, React.createElement(Overtime));
+      loadPage(`${pageName}-body`, React.createElement(Overtime));
       break;
     case 'ticketing':
-      safeLoad(`${pageName}-body`, React.createElement(Ticketing));
+      loadPage(`${pageName}-body`, React.createElement(Ticketing));
       break;
     case 'hyperlink':
-      safeLoad(`${pageName}-body`, React.createElement(Hyperlink));
+      loadPage(`${pageName}-body`, React.createElement(Hyperlink));
       break;
     case 'landing':
     default:
-      ReactDOM.createRoot(elementBody).render(<Landing />);
+      ReactDOM.createRoot(container).render(<Landing />);
       break;
   }
 });
-
-function setView(pageName: 'overtime' | 'ticketing' | 'hyperlink' | 'buttons' | 'landing') {
-  let element = document.querySelector(`#${pageName}-body`) as HTMLDivElement; //--|🠈 Select the new view element using its dynamic ID 🠈|--\\
-  switch (true) {
-    case element.classList.contains('asleep'):
-      //--|🠉 Show if element is hidden 🠈|--\\
-      element.classList.remove('asleep'); //--|🠈 Remove '.asleep' 🠈|--\\
-      console.log(`|🠊 Loaded: <div id="${pageName}-body"> 🠈|`);
-
-      return element.classList.add('active'); //--|🠈 Toggle '.active' 🠈|--\\
-    case element.classList.contains('active'):
-      //--|🠉 Optional toggle: allow hiding the current element again 🠈|--\\
-      element.classList.remove('active'); //--|🠈 Remove '.active' 🠈|--\\
-      return element.classList.add('asleep'); //--|🠈 Toggle '.asleep' 🠈|--\\
-  }
-}
-setView('overtime');
-
-function safeLoad(identification: string, container: React.ReactElement) {
-  let elementBody = document.getElementById(identification) as HTMLElement;
-  if (!elementBody) {
-    console.error(`Can't find #${identification}`);
-    return;
-  }
-  if (elementBody.innerHTML === '') {
-    ReactDOM.createRoot(elementBody).render(<React.StrictMode>{container}</React.StrictMode>);
-  } else {
-    console.warn(`Element #${identification} is not empty. Skipping render to avoid overwrite.`);
-  }
-}
