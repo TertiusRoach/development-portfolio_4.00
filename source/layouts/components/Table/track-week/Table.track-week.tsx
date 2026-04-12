fa93ba0867955a004772dca707b3cdb5b4798ca1
TertiusRoach <tertius.roach@gmail.com>
2026-04-12 14:11:39 +0200

    Test <tbody> duplication with the layout established in this SASS. #HTMLCoder

--- source/layouts/components/Table/track-week/Table.track-week.tsx
+++ source/layouts/components/Table/track-week/Table.track-week.tsx
@@ -20,51 +20,86 @@ const TrackWeek: React.FC<TheseProps> = ({ info }) => {
     <table className="track-week">
       <thead className="track-head">
         <tr>
-          <th>Mon</th>
-          <th>Tue</th>
-          <th>Wed</th>
-          <th>Thu</th>
-          <th>Fri</th>
-          <th>Sat</th>
-          <th>Sun</th>
+          <th className="mon-row">
+            <span>Mon</span>
+          </th>
+          <th className="tue-row">
+            <span>Tue</span>
+          </th>
+          <th className="wed-row">
+            <span>Wed</span>
+          </th>
+          <th className="thu-row">
+            <span>Thu</span>
+          </th>
+          <th className="fri-row">
+            <span>Fri</span>
+          </th>
+          <th className="sat-row">
+            <span>Sat</span>
+          </th>
+          <th className="sun-row">
+            <span>Sun</span>
+          </th>
         </tr>
       </thead>
 
       <tbody className="visible" id="current-week" data-week="02">
         <tr id="YYYY-DD-MM" className="mon-row">
-          {/* <td className="weekday h1">Mon</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">17:00</td> */}
+          <td className="mon_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="mon_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="tue-row">
-          {/* <td className="weekday h1">Tue</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">17:00</td> */}
+          <td className="tue_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="tue_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="wed-row">
-          {/* <td className="weekday h1">Wed</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">17:00</td> */}
+          <td className="wed_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="wed_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="thu-row">
-          {/* <td className="weekday h1">Thu</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">17:00</td> */}
+          <td className="thu_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="thu_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="fri-row">
-          {/* <td className="weekday h1">Fri</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">17:00</td> */}
+          <td className="fri_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="fri_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="sat-row">
-          {/* <td className="weekday h1">Sat</td> */}
-          {/* <td className="clock-in display-3">08:00</td> */}
-          {/* <td className="clock-out display-3">13:00</td> */}
+          <td className="sat_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sat_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
         <tr id="YYYY-DD-MM" className="sun-row">
-          {/* <td className="weekday h1">Sun</td> */}
-          {/* <td className="clock-in display-3">--:--</td> */}
-          {/* <td className="clock-out display-3">~~:~~</td> */}
+          <td className="sun_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sun_clock-out_am">
+            <span>17:00</span>
+          </td>
         </tr>
       </tbody>
 
