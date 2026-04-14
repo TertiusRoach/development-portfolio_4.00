3cc1c121b75271d15c3cba721d07e19ec8b6a983
Tertius Roach <63373022+TertiusRoach@users.noreply.github.com>
2026-04-14 15:55:21 +0200

    Reconsidering and splitting components for better scaling principles. _layout.scss     // grid + flex together _visual.scss     // tint, shadows, gradients _motion.scss     // transforms, transitions, carousel _letter.scss	// sizes, fonts

---
+++ source/layouts/components/Table/data-week/Table.data-week.tsx
@@ -0,0 +1,226 @@
+//--|🠊 Table.data-week.tsx 🠈|--\\
+//--|🠋 Styles 🠋|--\\
+import './Table.data-week.scss';
+//--|🠋 Dependencies 🠋|--\\
+import React, { useEffect } from 'react';
+
+interface TheseProps {
+  info: {
+    pageName: string;
+    blockName: string;
+  };
+}
+const DataWeek: React.FC<TheseProps> = ({ info }) => {
+  const blockName: string = info.blockName as 'leftbar';
+  const pageName: string = info.pageName as 'overtime';
+
+  useEffect(() => {}, [pageName, blockName]);
+
+  return (
+    <table className="track-week I">
+      {/* <thead className="track-head">
+        <tr>
+          <th className="mon-row">
+            <span>Mon</span>
+          </th>
+          <th className="tue-row">
+            <span>Tue</span>
+          </th>
+          <th className="wed-row">
+            <span>Wed</span>
+          </th>
+          <th className="thu-row">
+            <span>Thu</span>
+          </th>
+          <th className="fri-row">
+            <span>Fri</span>
+          </th>
+          <th className="sat-row">
+            <span>Sat</span>
+          </th>
+          <th className="sun-row">
+            <span>Sun</span>
+          </th>
+        </tr>
+      </thead> */}
+
+      {/* <tbody className="hidden" id="previous-week" data-week="01">
+        <tr id="YYYY-DD-MM" className="mon-row">
+          <td className="mon_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="mon_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="tue-row">
+          <td className="tue_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="tue_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="wed-row">
+          <td className="wed_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="wed_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="thu-row">
+          <td className="thu_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="thu_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="fri-row">
+          <td className="fri_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="fri_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sat-row">
+          <td className="sat_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sat_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sun-row">
+          <td className="sun_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sun_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+      </tbody> */}
+
+      {/* <tbody className="visible" id="current-week" data-week="02">
+        <tr id="YYYY-DD-MM" className="mon-row">
+          <td className="mon_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="mon_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="tue-row">
+          <td className="tue_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="tue_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="wed-row">
+          <td className="wed_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="wed_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="thu-row">
+          <td className="thu_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="thu_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="fri-row">
+          <td className="fri_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="fri_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sat-row">
+          <td className="sat_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sat_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sun-row">
+          <td className="sun_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sun_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+      </tbody> */}
+
+      {/* <tbody className="hidden" id="future-week" data-week="03">
+        <tr id="YYYY-DD-MM" className="mon-row">
+          <td className="mon_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="mon_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="tue-row">
+          <td className="tue_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="tue_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="wed-row">
+          <td className="wed_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="wed_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="thu-row">
+          <td className="thu_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="thu_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="fri-row">
+          <td className="fri_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="fri_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sat-row">
+          <td className="sat_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sat_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+        <tr id="YYYY-DD-MM" className="sun-row">
+          <td className="sun_clock-in_am">
+            <span>08:00</span>
+          </td>
+          <td className="sun_clock-out_am">
+            <span>17:00</span>
+          </td>
+        </tr>
+      </tbody> */}
+    </table>
+  );
+};
+export default DataWeek;
