84ba67002a67b2eaf9c71cac75596832afb3cba4
TertiusRoach <tertius.roach@gmail.com>
2026-06-14 09:41:30 +0200

    Built preview for <MenuSelect>

--- source/layouts/components/Menu/select/Menu_select.ts
+++ source/layouts/components/Menu/select/Menu_select.ts
@@ -20,35 +20,36 @@ export function markCarousel(
       const verticalCarousel = document.querySelector(
         `#${pageName}-main div[class="${labelName}-main_carousel-default"] ol[class="vert-Y-axis"] li[class*="carousel-vertical"]`,
       ) as HTMLElement;
-      const verticalController = document.querySelector(
-        `#${pageName}-${blockName} menu[class*="${labelName}-${blockName}_select-default"] ol[class="vert-Y-select"] li[class*="showing"]`,
-      ) as HTMLElement;
+      if (verticalCarousel !== null) {
+        const verticalController = document.querySelector(
+          `#${pageName}-${blockName} menu[class*="${labelName}-${blockName}_select-default"] ol[class="vert-Y-select"] li[class*="showing"]`,
+        ) as HTMLElement;
+        let slideShow: HTMLElement;
+        let slideCount = Array.from(verticalController.childNodes) as HTMLElement[];
+        let slideMark = romanToArabic(verticalCarousel.classList[0].split('_')[1]) as number;
 
-      let slideShow: HTMLElement;
-      let slideCount = Array.from(verticalController.childNodes) as HTMLElement[];
-      let slideMark = romanToArabic(verticalCarousel.classList[0].split('_')[1]) as number;
+        if (!slideCount.some((node) => node.classList.contains('downplay'))) {
+          setTimeout(() => {
+            if (showCases === 0) showCases = 1;
 
-      if (!slideCount.some((node) => node.classList.contains('downplay'))) {
-        setTimeout(() => {
-          if (showCases === 0) showCases = 1;
+            slideShow = verticalController.childNodes[showCases - 1] as HTMLElement;
+            var chainName = slideShow.classList[0].split('-')[0] as string;
 
-          slideShow = verticalController.childNodes[showCases - 1] as HTMLElement;
-          var chainName = slideShow.classList[0].split('-')[0] as string;
+            slideShow.classList.add('downplay');
+            slideShow.classList.remove('highlight');
+
+            selectCarousel(pageName, blockName, labelName, chainName, axisStyle);
+          }, 125);
+        } else {
+          slideShow = verticalController.childNodes[slideMark - 1] as HTMLElement;
+          var slideHide = slideCount.find((node) => node.classList.contains('downplay')) as HTMLElement;
+
+          slideHide.classList.add('highlight');
+          slideHide.classList.remove('downplay');
 
           slideShow.classList.add('downplay');
           slideShow.classList.remove('highlight');
-
-          selectCarousel(pageName, blockName, labelName, chainName, axisStyle);
-        }, 125);
-      } else {
-        slideShow = verticalController.childNodes[slideMark - 1] as HTMLElement;
-        var slideHide = slideCount.find((node) => node.classList.contains('downplay')) as HTMLElement;
-
-        slideHide.classList.add('highlight');
-        slideHide.classList.remove('downplay');
-
-        slideShow.classList.add('downplay');
-        slideShow.classList.remove('highlight');
+        }
       }
       break;
   }
@@ -74,29 +75,31 @@ export function selectCarousel(
       const verticalCarousel = document.querySelector(
         `#${pageName}-main div[class="${labelName}-main_carousel-default"] ol[class="vert-Y-axis"] li[class*="carousel-vertical"]`,
       ) as HTMLElement;
-      const verticalPreview = verticalCarousel.querySelector(`div[class="${labelName}-main_container"]`) as HTMLElement;
-      const verticalController = document.querySelector(
-        `#${pageName}-${blockName} menu[class*="${blockName}"] ol[class="vert-Y-select"] li[class*="showing-vertical"]`,
-      ) as HTMLElement;
 
-      for (let i = 0; i < verticalPreview.childElementCount; i++) {
-        let viewPage = verticalPreview.childNodes[i] as HTMLElement;
-        if (viewPage.classList[0].includes(chainName)) {
-          selectCarousel = arabicToRoman(i + 1) as string;
+      if (verticalCarousel !== null) {
+        const verticalPreview = verticalCarousel.querySelector(`div[class="${labelName}-main_container"]`) as HTMLElement;
+        const verticalController = document.querySelector(
+          `#${pageName}-${blockName} menu[class*="${blockName}"] ol[class="vert-Y-select"] li[class*="showing-vertical"]`,
+        ) as HTMLElement;
+        for (let i = 0; i < verticalPreview.childElementCount; i++) {
+          let viewPage = verticalPreview.childNodes[i] as HTMLElement;
+          if (viewPage.classList[0].includes(chainName)) {
+            selectCarousel = arabicToRoman(i + 1) as string;
 
-          verticalController.classList.remove(verticalController.classList[0]);
-          verticalController.classList.add(`showing-vertical_${selectCarousel}`);
+            verticalController.classList.remove(verticalController.classList[0]);
+            verticalController.classList.add(`showing-vertical_${selectCarousel}`);
 
-          returnPreview = i + 1;
-          break;
+            returnPreview = i + 1;
+            break;
+          }
         }
-      }
-      prevView = verticalCarousel.classList[0] as string;
-      nextView = `carousel-vertical_${selectCarousel}` as string;
+        prevView = verticalCarousel.classList[0] as string;
+        nextView = `carousel-vertical_${selectCarousel}` as string;
 
-      if (prevView !== nextView) {
-        verticalCarousel.classList.add(nextView);
-        verticalCarousel.classList.remove(prevView);
+        if (prevView !== nextView) {
+          verticalCarousel.classList.add(nextView);
+          verticalCarousel.classList.remove(prevView);
+        }
       }
       break;
   }
--- source/layouts/containers/Archive/Main/elements/menu-components/select-menu/SelectMenu.tsx
+++ source/layouts/containers/Archive/Main/elements/menu-components/select-menu/SelectMenu.tsx
@@ -6,6 +6,7 @@ import './SelectMenu.scss';
 
 //--|🠋 Functions 🠋|--\\
 import { stripBrackets } from '../../../../../../../scripts';
+import MenuSelect from '../../../../../../components/Menu/select/Menu.select';
 
 //--|🠋 Components 🠋|--\\
 
@@ -21,9 +22,96 @@ const SelectMenu: React.FC<InfoProps> = ({ info }) => {
   const labelName = info.labelName as 'default';
   const pageName = info.pageName as 'component';
 
+  useEffect(() => {}, [pageName, blockName, labelName]);
+
+  let link: string =
+    'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/c4b15354f49365eb0328a4036df8612eff1e8b64';
+  let view: Array<{ labelName: string; imageLink: string }> = [
+    {
+      labelName: 'overtime',
+      imageLink: `${link}/source/assets/svg-files/archive-images/trinity-apps/track-a-day/primary-medium.svg` as string,
+    },
+    {
+      labelName: 'ticketing',
+      imageLink: `${link}/source/assets/svg-files/archive-images/trinity-apps/log-a-ticket/primary-medium.svg` as string,
+    },
+    {
+      labelName: 'hyperlink',
+      imageLink: `${link}/source/assets/svg-files/archive-images/trinity-apps/find-a-link/primary-medium.svg` as string,
+    },
+  ];
   return (
     <aside className="select-menu">
-      <section className={`${blockName}-foreground`}></section>
+      <section className={`${blockName}-foreground`}>
+        <MenuSelect
+          info={{
+            labelName: 'menu' as string,
+            blockName: blockName as 'main',
+            pageName: pageName as 'components',
+          }}
+          style={{
+            axis: '[x]',
+            color: '(mono)',
+            view: 'top-cen',
+            shade: '~dark~',
+          }}
+          cases={{
+            show: 0,
+            buttons: view as Array<{ labelName: string; imageLink: string }>,
+          }}
+        />
+        <MenuSelect
+          info={{
+            labelName: 'menu' as string,
+            blockName: blockName as 'main',
+            pageName: pageName as 'components',
+          }}
+          style={{
+            axis: '[y]',
+            color: '(mono)',
+            view: 'mid-rig',
+            shade: '~light~',
+          }}
+          cases={{
+            show: 0,
+            buttons: view as Array<{ labelName: string; imageLink: string }>,
+          }}
+        />
+        <MenuSelect
+          info={{
+            labelName: 'menu' as string,
+            blockName: blockName as 'main',
+            pageName: pageName as 'components',
+          }}
+          style={{
+            axis: '[x]',
+            color: '(mono)',
+            view: 'bot-cen',
+            shade: '~light~',
+          }}
+          cases={{
+            show: 0,
+            buttons: view as Array<{ labelName: string; imageLink: string }>,
+          }}
+        />
+        <MenuSelect
+          info={{
+            labelName: 'menu' as string,
+            blockName: blockName as 'main',
+            pageName: pageName as 'components',
+          }}
+          style={{
+            axis: '[y]',
+            color: '(mono)',
+            view: 'mid-lef',
+            shade: '~dark~',
+          }}
+          cases={{
+            show: 0,
+            buttons: view as Array<{ labelName: string; imageLink: string }>,
+          }}
+        />
+      </section>
       <figure className={`${blockName}-midground`}></figure>
       <div className={`${blockName}-background`}>
         <h1 className="display-1">{`<SelectMenu>`}</h1>
