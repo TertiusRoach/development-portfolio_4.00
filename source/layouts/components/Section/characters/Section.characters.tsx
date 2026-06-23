07096b3b6d57225f32a713629be63f158729b31e
TertiusRoach <tertius.roach@gmail.com>
2026-06-23 16:25:17 +0200

    Designed 70% of the character card.

--- source/layouts/components/Section/characters/Section.characters.tsx
+++ source/layouts/components/Section/characters/Section.characters.tsx
@@ -3,6 +3,8 @@
 import './Section.characters.scss';
 
 //--|🠋 Functions 🠋|--\\
+import { returnProfiles } from './Section_characters';
+import type { CharacterReferences } from './Section_characters';
 
 //--|🠋 Components 🠋|--\\
 
@@ -15,12 +17,8 @@ interface TheseProps {
     blockName: string;
     labelName: string;
   };
-}
-type CharacterReferences = {
-  key: [
-    number,
-    (
-      | string
+  cases: {
+    profile:
       | 'jane'
       | 'malik'
       | 'dimitri'
@@ -38,38 +36,10 @@ type CharacterReferences = {
       | 'nyra'
       | 'victor'
       | 'danish'
-      | 'aelin'
-    ),
-  ];
-  fullName:
-    | 'Jane Lester'
-    | 'Malik Tremaine Carter'
-    | 'Dimitri Lewis'
-    | 'Dale Sutton'
-    | 'Dr. Alaric Voss'
-    | 'Conrad Guy'
-    | 'Daniel Meyers'
-    | 'Ms. Kady Deacon'
-    | "Seamus O'Donnell"
-    | 'Hammad Dean'
-    | 'Tasneem Kemp'
-    | 'Sipho Dlamini'
-    | 'Elliot Crane'
-    | 'Zuberi Thorne'
-    | 'Nyra Solari'
-    | 'Victor Langston'
-    | 'Danish Copeland'
-    | 'Aelin Darrow';
-  country: string;
-  department: [boolean, string];
-  description: string;
-  profile: string;
-  permissions: [
-    { vocation: 'Established' | 'Freelancing' },
-    { position: 'Employee' | 'Manager' },
-    { occupation: 'Specialist' | 'Technician' },
-  ];
-};
+      | 'aelin';
+  };
+}
+
 const SectionCharacters: React.FC<TheseProps> = ({ info }) => {
   const pageName = info.pageName as string;
   const blockName = info.blockName as string;
@@ -80,209 +50,68 @@ const SectionCharacters: React.FC<TheseProps> = ({ info }) => {
   switch (pageName) {
     case 'components':
     default:
-      return (
-        <section className={`${info.labelName}-${info.blockName}_characters-default`}>
-          <div className="wrapper">
-            <header className="information">
-              <img src="" alt="" />
-              <h1 className="full-name">full-name</h1>
-              <p className="country">South Africa</p>
-            </header>
-            <section className="permissions">
-              <div className="vocation">
-                <h6>Vocation</h6>
-                <p>Established or Freelancing</p>
-              </div>
-              <div className="position">
-                <h6>Position</h6>
-                <p>Employee or Manager</p>
-              </div>
-              <div className="occupation">
-                <h6>Occupation</h6>
-                <p>Specialist or Technician</p>
-              </div>
-            </section>
-            <footer className="description">
-              <h3 className="department">Operational</h3>
-              <p className="description">LOREMIPSUM!</p>
-            </footer>
-          </div>
-        </section>
-      );
-  }
-  if (pageName === 'components') {
+      let jane = returnProfiles(pageName as 'components', blockName as 'main')[0] as CharacterReferences | HTMLElement;
+      let mali = returnProfiles(pageName as 'components', blockName as 'main')[1] as CharacterReferences | HTMLElement;
+      let dimi = returnProfiles(pageName as 'components', blockName as 'main')[2];
+      let dale = returnProfiles(pageName as 'components', blockName as 'main')[3];
+      let alar = returnProfiles(pageName as 'components', blockName as 'main')[4];
+      let conr = returnProfiles(pageName as 'components', blockName as 'main')[5];
+      let dani = returnProfiles(pageName as 'components', blockName as 'main')[6];
+      let kady = returnProfiles(pageName as 'components', blockName as 'main')[7];
+      let seam = returnProfiles(pageName as 'components', blockName as 'main')[8];
+      let hamm = returnProfiles(pageName as 'components', blockName as 'main')[9];
+      let tasn = returnProfiles(pageName as 'components', blockName as 'main')[10];
+      let elli = returnProfiles(pageName as 'components', blockName as 'main')[11];
+      let siph = returnProfiles(pageName as 'components', blockName as 'main')[12];
+      let zube = returnProfiles(pageName as 'components', blockName as 'main')[13];
+      let nyra = returnProfiles(pageName as 'components', blockName as 'main')[14];
+      let vict = returnProfiles(pageName as 'components', blockName as 'main')[15];
+      let cope = returnProfiles(pageName as 'components', blockName as 'main')[16];
+      let aeli = returnProfiles(pageName as 'components', blockName as 'main')[17];
+
+      if ('profile' in jane) {
+        let imageLink = jane.profile as string;
+        let fullName = jane.fullName as string;
+        let country = jane.country as string;
+
+        let vocation = jane.permissions[0].vocation;
+        let position = jane.permissions[1].position;
+        let occupation = jane.permissions[2].occupation;
+
+        return (
+          <section className={`${info.labelName}-${info.blockName}_characters-default`}>
+            <div className="wrapper">
+              <header className="information">
+                <div className="profile">
+                  <img src={imageLink} alt="" />
+                </div>
+                <h1 className="full-name">{fullName}</h1>
+                <p className="country">{country}</p>
+              </header>
+              <section className="permissions">
+                <div className="vocation">
+                  <h6>Vocation</h6>
+                  <p>Established</p>
+                </div>
+                <div className="position">
+                  <h6>Position</h6>
+                  <p>Manager</p>
+                </div>
+                <div className="occupation">
+                  <h6>Occupation</h6>
+                  <p>Specialist</p>
+                </div>
+              </section>
+              <footer className="description">
+                <h3 className="department">Operational</h3>
+                <p className="description">LOREMIPSUM!</p>
+              </footer>
+            </div>
+          </section>
+        );
+      }
   }
 };
 export default SectionCharacters;
 
-let randomizeCharacters = (pageName: 'components', blockName: 'main'): Array<CharacterReferences | HTMLElement> => {
-  //--|🠋 Characters 🠋|--\\
-  let characters: Array<CharacterReferences> = [
-    {
-      fullName: 'Jane Lester',
-      department: [true, 'Owner/CEO'],
-      country: 'United States (Silicon Valley Origin)',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [1, 'jane'],
-    },
-    {
-      fullName: 'Malik Tremaine Carter',
-      department: [false, 'Coordination'],
-      country: 'Canada',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [2, 'malik'],
-    },
-    {
-      fullName: 'Dimitri Lewis',
-      department: [false, 'Technical'],
-      country: 'Greece',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [3, 'dimitri'],
-    },
-    {
-      fullName: 'Dale Sutton',
-      department: [false, 'Technical'],
-      country: 'Canada',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [4, 'dale'],
-    },
-    {
-      fullName: 'Dr. Alaric Voss',
-      department: [true, 'Technical'],
-      country: 'Germany',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [5, 'alaric'],
-    },
-    {
-      fullName: 'Conrad Guy',
-      department: [true, 'Admin'],
-      country: 'South Africa',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [6, 'conrad'],
-    },
-    {
-      fullName: 'Daniel Meyers',
-      department: [false, 'Admin'],
-      country: 'United Kingdom',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [7, 'daniel'],
-    },
-    {
-      fullName: 'Ms. Kady Deacon',
-      department: [false, 'Admin'],
-      country: 'United Kingdom',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [8, 'kady'],
-    },
-    {
-      fullName: "Seamus O'Donnell",
-      department: [true, 'Maintenance'],
-      country: 'Ireland',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [9, 'seamus'],
-    },
-    {
-      fullName: 'Hammad Dean',
-      department: [true, 'Technical'],
-      country: 'United Arab Emirates',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [10, 'hammad'],
-    },
-    {
-      fullName: 'Tasneem Kemp',
-      department: [false, 'Maintenance'],
-      country: 'India',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [11, 'tasneem'],
-    },
-    {
-      fullName: 'Sipho Dlamini',
-      department: [false, 'Maintenance'],
-      country: 'South Africa',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [12, 'elliot'],
-    },
-    {
-      fullName: 'Elliot Crane',
-      department: [true, 'Operational'],
-      country: 'United States',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [13, 'sipho'],
-    },
-    {
-      fullName: 'Zuberi Thorne',
-      department: [false, 'Operational'],
-      country: 'South Africa',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [14, 'zuberi'],
-    },
-    {
-      fullName: 'Nyra Solari',
-      department: [false, 'Operational'],
-      country: 'China',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [15, 'nyra'],
-    },
-    {
-      fullName: 'Victor Langston',
-      department: [false, 'Operational'],
-      country: 'United States',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [16, 'victor'],
-    },
-    {
-      fullName: 'Danish Copeland',
-      department: [true, 'Human Resources'],
-      country: 'Australia',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [17, 'danish'],
-    },
-    {
-      fullName: 'Aelin Darrow',
-      department: [true, 'Coordination'],
-      country: 'United Kingdom',
-      description: '',
-      profile: '',
-      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
-      key: [18, 'aelin'],
-    },
-  ];
-
-  //--|🠋 HTML Element (always last) 🠋|--\\
-  let element: HTMLElement = document.querySelector(`#${pageName}-${blockName}`) as HTMLElement;
-  return [...characters, element];
-};
+let randomizeCharacter = () => {};
--- source/layouts/components/Section/characters/Section_characters.ts
+++ source/layouts/components/Section/characters/Section_characters.ts
@@ -1,2 +1,229 @@
 //--|🠊 Section_characters.ts 🠈|--\\
 //--|🠋 Styles 🠋|--\\
+export type CharacterReferences = {
+  key: [
+    number,
+    (
+      | string
+      | 'jane'
+      | 'malik'
+      | 'dimitri'
+      | 'dale'
+      | 'alaric'
+      | 'conrad'
+      | 'daniel'
+      | 'kady'
+      | 'seamus'
+      | 'hammad'
+      | 'tasneem'
+      | 'elliot'
+      | 'sipho'
+      | 'zuberi'
+      | 'nyra'
+      | 'victor'
+      | 'danish'
+      | 'aelin'
+    ),
+  ];
+  fullName:
+    | 'Jane Lester'
+    | 'Malik Tremaine Carter'
+    | 'Dimitri Lewis'
+    | 'Dale Sutton'
+    | 'Dr. Alaric Voss'
+    | 'Conrad Guy'
+    | 'Daniel Meyers'
+    | 'Ms. Kady Deacon'
+    | "Seamus O'Donnell"
+    | 'Hammad Dean'
+    | 'Tasneem Kemp'
+    | 'Sipho Dlamini'
+    | 'Elliot Crane'
+    | 'Zuberi Thorne'
+    | 'Nyra Solari'
+    | 'Victor Langston'
+    | 'Danish Copeland'
+    | 'Aelin Darrow';
+  country: string;
+  department: [boolean, string];
+  description: string;
+  profile: string;
+  permissions: [
+    { vocation: 'Established' | 'Freelancing' },
+    { position: 'Employee' | 'Manager' },
+    { occupation: 'Specialist' | 'Technician' },
+  ];
+};
+export let returnProfiles = (pageName: 'components', blockName: 'main'): Array<CharacterReferences | HTMLElement> => {
+  //--|🠋 Characters 🠋|--\\
+  let characters: Array<CharacterReferences> = [
+    {
+      fullName: 'Jane Lester',
+      department: [true, 'Owner/CEO'],
+      country: 'United States',
+      description: '',
+      profile:
+        'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/refs/heads/main/source/assets/png-files/archive-images/tralogfin-application/demonstration/original/jane-lester.png',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [1, 'jane'],
+    },
+    {
+      fullName: 'Malik Tremaine Carter',
+      department: [false, 'Coordination'],
+      country: 'Canada',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [2, 'malik'],
+    },
+    {
+      fullName: 'Dimitri Lewis',
+      department: [false, 'Technical'],
+      country: 'Greece',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [3, 'dimitri'],
+    },
+    {
+      fullName: 'Dale Sutton',
+      department: [false, 'Technical'],
+      country: 'Canada',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [4, 'dale'],
+    },
+    {
+      fullName: 'Dr. Alaric Voss',
+      department: [true, 'Technical'],
+      country: 'Germany',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [5, 'alaric'],
+    },
+    {
+      fullName: 'Conrad Guy',
+      department: [true, 'Admin'],
+      country: 'South Africa',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [6, 'conrad'],
+    },
+    {
+      fullName: 'Daniel Meyers',
+      department: [false, 'Admin'],
+      country: 'United Kingdom',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [7, 'daniel'],
+    },
+    {
+      fullName: 'Ms. Kady Deacon',
+      department: [false, 'Admin'],
+      country: 'United Kingdom',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [8, 'kady'],
+    },
+    {
+      fullName: "Seamus O'Donnell",
+      department: [true, 'Maintenance'],
+      country: 'Ireland',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [9, 'seamus'],
+    },
+    {
+      fullName: 'Hammad Dean',
+      department: [true, 'Technical'],
+      country: 'United Arab Emirates',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [10, 'hammad'],
+    },
+    {
+      fullName: 'Tasneem Kemp',
+      department: [false, 'Maintenance'],
+      country: 'India',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [11, 'tasneem'],
+    },
+    {
+      fullName: 'Sipho Dlamini',
+      department: [false, 'Maintenance'],
+      country: 'South Africa',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [12, 'elliot'],
+    },
+    {
+      fullName: 'Elliot Crane',
+      department: [true, 'Operational'],
+      country: 'United States',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [13, 'sipho'],
+    },
+    {
+      fullName: 'Zuberi Thorne',
+      department: [false, 'Operational'],
+      country: 'South Africa',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [14, 'zuberi'],
+    },
+    {
+      fullName: 'Nyra Solari',
+      department: [false, 'Operational'],
+      country: 'China',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [15, 'nyra'],
+    },
+    {
+      fullName: 'Victor Langston',
+      department: [false, 'Operational'],
+      country: 'United States',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [16, 'victor'],
+    },
+    {
+      fullName: 'Danish Copeland',
+      department: [true, 'Human Resources'],
+      country: 'Australia',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [17, 'danish'],
+    },
+    {
+      fullName: 'Aelin Darrow',
+      department: [true, 'Coordination'],
+      country: 'United Kingdom',
+      description: '',
+      profile: '',
+      permissions: [{ vocation: 'Established' }, { position: 'Employee' }, { occupation: 'Specialist' }],
+      key: [18, 'aelin'],
+    },
+  ];
+
+  //--|🠋 HTML Element (always last) 🠋|--\\
+  let element: HTMLElement = document.querySelector(`#${pageName}-${blockName}`) as HTMLElement;
+  return [...characters, element];
+};
+let returnUsers = (pageName: 'components', blockName: 'main', labelName: string) => {};
