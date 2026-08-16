471960ea4d6698d79dca7995ac411478bf124649
TertiusRoach <tertius.roach@gmail.com>
2026-08-16 10:23:02 +0200

    This is a baseline function for image retrieval. Keep it separate and implement it chronologically as the project scales.

--- source/layouts/scripts/archive.ts
+++ source/layouts/scripts/archive.ts
@@ -1,8 +1,18 @@
 //--|🠊 archive.ts 🠈|--\\
 function loadAsset(file: '-gif-' | '-ico-' | '-jpg-' | '-pdf-' | '-png-' | '-svg-', path: string): string {
-  //   alert('LINKED!!!!');
-  //   console.log('LINKED!!!');
-  return '';
-}
+  //--|🠊 Finds path for file extension 🠈|--\\
+  const assetSource: string =
+    'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_4.00/refs/heads/main/source/assets';
 
+  //--|🠊 Build URL string 🠈|--\\
+  let filePath: Record<'-gif-' | '-ico-' | '-jpg-' | '-pdf-' | '-png-' | '-svg-', [string, string]> = {
+    '-gif-': [`${assetSource}/gif-files`, '.gif'],
+    '-ico-': [`${assetSource}/ico-files`, '.ico'],
+    '-jpg-': [`${assetSource}/jpg-files`, '.jpg'],
+    '-pdf-': [`${assetSource}/pdf-files`, '.pdf'],
+    '-png-': [`${assetSource}/png-files`, '.png'],
+    '-svg-': [`${assetSource}/svg-files`, '.svg'],
+  };
+  return (filePath[file][0] + path + filePath[file][1]) as string;
+}
 export default loadAsset;
