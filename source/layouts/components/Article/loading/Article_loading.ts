925a5e37a030a9be0efe2171e77e523907564385
Tertius Roach <tertius.roach@gmail.com>
2026-09-14 16:25:56 +0200

    loadAsset clashes with something that's exported as a default. Project is getting too big for me to remember where everything is.

--- source/layouts/components/Article/loading/Article_loading.ts
+++ source/layouts/components/Article/loading/Article_loading.ts
@@ -1,8 +1,7 @@
 //--|🠊 Article_loading.ts 🠈|--\\
-import loadAsset from '../../../../scripts';
+import { loadAsset } from '../../../../scripts';
 
 function loadShade(apps: '{signature}' | '{tralogfin}', shade: '~dark~' | '~light~'): string {
-  const typeName = '-gif-';
   const pathName = {
     '{signature}': {
       '~dark~': '/signature/1280x720%2C%2015fps/signature-dark',
@@ -12,9 +11,7 @@ function loadShade(apps: '{signature}' | '{tralogfin}', shade: '~dark~' | '~ligh
       '~dark~': '/trinity-apps/1280x720%2C 15fps/1280x720%2C 15fps_black',
       '~light~': '/trinity-apps/1280x720%2C 15fps/1280x720%2C 15fps_white',
     },
-  };
-
-  // return loadAsset(typeName, pathName[apps][shade]) as string;
-  return '';
+  } as const;
+  return loadAsset('-gif-', pathName[apps][shade]) as string;
 }
 export default loadShade;
